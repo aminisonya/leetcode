aa4e65579e9cd2fd162ef2af4e3cdfc28ea40172
Sonya <aminisonya@gmail.com>
2022-02-19 19:07:49 -0800

    Time: 154 ms (34.99%), Space: 40.8 MB (46.15%) - LeetHub

---
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.cs
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public int KthSmallest(TreeNode root, int k) {
+        var stack = new Stack<TreeNode>();
+		
+		while (true)
+		{
+			while (root != null)
+			{
+				stack.Push(root);
+				root = root.left;
+			}
+			root = stack.Pop();
+			k--;
+			if (k == 0) return root.val;
+			root = root.right;
+		}
+    }
+}
