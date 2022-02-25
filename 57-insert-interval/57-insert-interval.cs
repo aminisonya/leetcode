d752daf7d319a47c9a0ce7feae06e85d8fb1f425
Sonya <aminisonya@gmail.com>
2022-02-24 21:24:28 -0800

    Time: 267 ms (13.28%), Space: 45 MB (19.02%) - LeetHub

--- 57-insert-interval/57-insert-interval.cs
+++ 57-insert-interval/57-insert-interval.cs
@@ -1,6 +1,8 @@
 public class Solution {
     public int[][] Insert(int[][] intervals, int[] newInterval) {
-        var result = new List<int[]>();
+        // Broken into 3 steps
+		// 1. Add all intervals before the new interval starts
+		var result = new List<int[]>();
 		var i = 0;
 		
 		while (i < intervals.Length && intervals[i][1] < newInterval[0])
@@ -9,14 +11,18 @@ public class Solution {
 			i++;
 		}
 		
+		// 2. Add new interval, after merging any overlaps
 		while (i < intervals.Length && intervals[i][0] <= newInterval[1])
 		{
-			newInterval[0] = Math.Min(newInterval[0], intervals[i][0]);
-			newInterval[1] = Math.Max(newInterval[1], intervals[i][1]);
+			// Update values of interval to be added, that merges all potential overlaps
+			newInterval[0] = Math.Min(intervals[i][0], newInterval[0]);
+			newInterval[1] = Math.Max(intervals[i][1], newInterval[1]);
 			i++;
 		}
+		// Add new interval outside of while loop, so it's only added once
 		result.Add(newInterval);
 		
+		// 3. Add any remaining intervals
 		while (i < intervals.Length)
 		{
 			result.Add(intervals[i]);
