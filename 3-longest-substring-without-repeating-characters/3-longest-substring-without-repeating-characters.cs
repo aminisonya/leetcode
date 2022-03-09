d7e2a74d72bc887a15d9ded60c7513bac4243728
Sonya <aminisonya@gmail.com>
2022-03-09 00:02:51 -0800

    Time: 97 ms (68.69%), Space: 36.8 MB (66.99%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cs
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cs
@@ -1,7 +1,8 @@
 public class Solution {
     public int LengthOfLongestSubstring(string s) {
-        // use dict to keep track of CURRENT chars
-        // sliding window
+        // Sliding window method
+        // Use dictionary to keep track of current chars in window
+        // When you see a repeating char, remove from dictionary and update/slide window
         
         var dict = new Dictionary<char, char>();
         var left = 0;
@@ -12,16 +13,23 @@ public class Solution {
         {
             if (!dict.ContainsKey(s[right]))
             {
+                // If it's not a repeating char
+                // Add to dictionary of current chars
+                // Iterate right side of window to expand
                 dict.Add(s[right], s[right]);
                 right++;
                 result = Math.Max(result, right - left);
             }
             else
             {
+                // If it IS a repeating character
+                // Remove far left from dictionary of current chars
+                // Iterate left to adjust starting point of window
                 dict.Remove(s[left]);
                 left++;
             }
         }
+        
         return result;
     }
 }
