243c500065478ebe1eec05cc752a28d276d2bf95
Sonya <aminisonya@gmail.com>
2022-03-11 00:03:19 -0800

    Time: 90 ms (71.19%), Space: 36.2 MB (57.42%) - LeetHub

---
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cs
@@ -0,0 +1,37 @@
+public class Solution {
+    public int CharacterReplacement(string s, int k) {
+        // Sliding window
+        // As we "slide", we need to know how many letters in our substring we need to replace
+        // You take the size of the window minus the most frequent letter that is in that window
+        // Once we know how many letters need to be replaced, we can compare it to given int "k" and adjust window if necessary
+        
+        // Array for all 26 letters
+        var freq = new int[26];
+        var mostFreqLetter = 0;
+        var left = 0;
+        var max = 0;
+        
+        for (var right = 0; right < s.Length; right++)
+        {
+            freq[s[right] - 'A']++;
+            mostFreqLetter = Math.Max(mostFreqLetter, freq[s[right] - 'A']);
+            
+            // As we "slide", we need to know how many letters in our substring we need to replace.
+            // We do this by taking the size of the window minus the most frequent letter that is in that window.
+            var lettersToChange = (right - left + 1) - mostFreqLetter;
+            
+            // Once we know how many letters need to be replaced, we can compare it to given int "k" and adjust window if necessary
+            // If letters that need to be replaced is > k, we adjust our window
+            if (lettersToChange > k)
+            {
+                freq[s[left] - 'A']--;
+                left++;
+            }
+            
+            // Get max between current window and previous max value
+            max = Math.Max(max, right - left + 1);
+        }
+        
+        return max;
+    }
+}
