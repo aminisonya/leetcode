a1f2661737cfea8a6a32c85edce651cd7bbfe3fd
Sonya <aminisonya@gmail.com>
2022-03-12 21:07:18 -0800

    Time: 384 ms (11.30%), Space: 50.9 MB (29.77%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.cs
+++ 49-group-anagrams/49-group-anagrams.cs
@@ -7,22 +7,44 @@ public class Solution {
             return result;
         }
         
+        // Create dictionary to store a character count string and a list for all strings that fit that character count
         var dict = new Dictionary<string, List<string>>();
         
+        // Loop thru strings
         foreach (var str in strs)
         {
+            // We want to keep count of how many times each char appears in each string
             var chars = str.ToCharArray();
-            Array.Sort(chars);
-            var sortedStr = new string(chars);
+            var charArr = new int[26];
             
-            if (!dict.ContainsKey(sortedStr))
+            foreach (var c in chars)
             {
-                dict.Add(sortedStr, new List<string>());
+                // Subtract 'a' to get the ASCII number value for that character (for lowercase)
+                var curr = c - 'a';
+                charArr[curr]++;
             }
             
-            dict[sortedStr].Add(str);
+            // Translate the int array into a string to be used as a key in our dictionary later
+            var sb = new StringBuilder();
+            for (var i = 0; i < 26; i++)
+            {
+                sb.Append('#');
+                sb.Append(charArr[i]);
+            }
+            var strKey = sb.ToString();
+            
+            // Add if it doesn't already exist in our dictionary
+            if (!dict.ContainsKey(strKey))
+            {
+                dict.Add(strKey, new List<string>());
+            }
+            
+            // Add the current string from our list of strings to the dictionary
+            // All strings with the same character count will be added to the same key in the dict
+            dict[strKey].Add(str);
         }
         
+        // Loop thru dictionary and add all values to result set
         foreach (var item in dict.Keys)
         {
             result.Add(dict[item]);
