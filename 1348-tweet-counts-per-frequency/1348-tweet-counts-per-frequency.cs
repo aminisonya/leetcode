d2450ab3a43db818f693e6147fe522efdf5683e6
Sonya <aminisonya@gmail.com>
2022-03-17 23:53:32 -0700

    Time: 457 ms (43.90%), Space: 60.1 MB (92.68%) - LeetHub

--- 1348-tweet-counts-per-frequency/1348-tweet-counts-per-frequency.cs
+++ 1348-tweet-counts-per-frequency/1348-tweet-counts-per-frequency.cs
@@ -1,5 +1,6 @@
 public class TweetCounts {
 
+    // Create a dictionary at class level to store tweets along with a list of timestamps
     private Dictionary<string, List<int>> Tweets;
 
     public TweetCounts() {        
@@ -7,16 +8,18 @@ public class TweetCounts {
     }
     
     public void RecordTweet(string tweetName, int time) {
+        // If a new tweet comes in, add it to dictionary
         if (!Tweets.ContainsKey(tweetName))
         {
             Tweets.Add(tweetName, new List<int>());
         }
         
+        // Add timestamp to tweet in dictionary
         Tweets[tweetName].Add(time);
     }
     
     public IList<int> GetTweetCountsPerFrequency(string freq, string tweetName, int startTime, int endTime) {
-        //int delta = deltas[freq];
+        // Calculate the delta based on passed in "freq" string
         var delta = 1;
         
         if (freq == "minute")
@@ -32,13 +35,24 @@ public class TweetCounts {
             delta = 60 * 60 * 24;
         }
         
-        var ans = new List<int>(new int[1 + (endTime - startTime) / delta]);
+        // Calculate how many time chunks there are between this start and end time, based on frequency
+        var timeChunk = 1 + (endTime - startTime) / delta;
         
-        foreach (int time in Tweets[tweetName])
+        // Instantiate array with length based on how many time chunks there are
+        var result = new int[timeChunk];
+        
+        // Iterate thru timestamps for this tweet
+        // If tweet happened during time frame between start and end times, add 1 to correct slot in array based on time chunks breakdown
+        foreach (var time in Tweets[tweetName])
+        {
             if (time >= startTime && time <= endTime)
-                ans[(time - startTime) / delta]++;
+            {
+                var i = (time - startTime) / delta;
+                result[i]++;
+            }
+        }
         
-        return ans;
+        return result.ToList();
     }
 }
 
