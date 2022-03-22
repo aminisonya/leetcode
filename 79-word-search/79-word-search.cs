dfbbfae73d557d7273f69a5c2a61b0a1e651ff1e
Sonya <aminisonya@gmail.com>
2022-03-22 14:38:10 -0700

    Time: 272 ms (84.84%), Space: 44.7 MB (5.99%) - LeetHub

--- 79-word-search/79-word-search.cs
+++ 79-word-search/79-word-search.cs
@@ -1,50 +1,67 @@
 public class Solution {
     public bool Exist(char[][] board, string word) {
-    for (var i = 0; i < board.Length; i++)
-    {
-      for (var j = 0; j < board[i].Length; j++)
-      {
-        // Backtracking
-        if (Backtrack(board, i, j, word, 0))
+        // Backtracking recursion
+        // If next letter is not next letter in word we're searching for, backtrack
+        // Traverse matrix starting from top left
+        // As we traverse, want to mark visited cells somehow
+        
+        for (var i = 0; i < board.Length; i++)
         {
-          return true;
+            for (var j = 0; j < board[i].Length; j++)
+            {
+                if (Backtrack(board, i, j, 0, word))
+                {
+                    return true;
+                }
+            }
         }
-      }
+        
+        return false;
     }
     
-    return false;
-  }
-
-  public bool Backtrack(char[][] board, int row, int col, string word, int index)
-  {
-    // Have we found all letters in the word?
-    if (index >= word.Length)
+    public bool Backtrack(char[][] board, int row, int col, int index, string word)
     {
-      return true;
+        // Check if we have surpassed letters in word, that means we've found the word
+        if (index >= word.Length)
+        {
+            return true;
+        }
+        
+        // Check if we are out of bounds of matrix
+        if (row < 0 || col < 0 || row >= board.Length || col >= board[0].Length)
+        {
+            return false;
+        }
+        
+        // Check if current place in board is equal to next letter in word we're searching for
+        if (board[row][col] != word[index])
+        {
+            return false;
+        }
+        
+        // Mark current place in board so as not to doubly visit it
+        // Keep hold of value in temp variable in case it doesn't work out
+        var temp = board[row][col];
+        board[row][col] = '#';        
+        
+        var result = false;
+        
+        // Want to go in clockwise order, checking next candidates
+        var rowsToCheck = new int[4] {1, 0, -1, 0};
+        var colsToCheck = new int[4] {0, 1, 0, -1};
+        
+        for (var i = 0; i < 4; i++)
+        {
+            result = Backtrack(board, row + rowsToCheck[i], col + colsToCheck[i], index + 1, word);
+            
+            if (result == true)
+            {
+                break;
+            }
+        }
+        
+        // If wasn't valid move, change letter back to letter from '#' placeholder
+        board[row][col] = temp;
+        return result;
     }
-
-    // FIRST ensure we aren't out of bounds of board
-    // Then check if current letter matches place in word
-    if (row < 0 || row >= board.Length || col < 0 || col >= board[0].Length || board[row][col] != word[index])
-    {
-      return false;
-    }
-
-    // Mark current place
-    // Search all directions 
-    board[row][col] = '#';
-
-    var rowOffsets = new int[] {0, 1, 0, -1};
-    var colOffsets = new int[] {1, 0, -1, 0};
-    for (var i = 0; i < 4; i++)
-    {
-      if (Backtrack(board, row + rowOffsets[i], col + colOffsets[i], word, index + 1)) 
-      {
-        return true;
-      }
-    }
-
-    board[row][col] = word[index];
-    return false;
-  }
 }
