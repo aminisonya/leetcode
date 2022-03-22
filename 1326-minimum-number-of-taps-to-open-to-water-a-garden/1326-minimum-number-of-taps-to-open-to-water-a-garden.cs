789999dcfb8f6402ac3771e766cc8a658dc4b809
Sonya <aminisonya@gmail.com>
2022-03-21 17:17:48 -0700

    Time: 104 ms (80.33%), Space: 41.8 MB (26.23%) - LeetHub

--- 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.cs
+++ 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.cs
@@ -1,9 +1,11 @@
 public class Solution {
     public int MinTaps(int n, int[] ranges) {
+        // Bit of a brute force solution
         var min = 0;
         var max = 0;
         var count = 0;
         
+        // Basically, how many times do we have to update max before we've watered whole garden?
         while (max < n)
         {
             // Choose the tap with max range to the right, for each given value to the left
@@ -16,13 +18,14 @@ public class Solution {
             }
             
             // If max did not change, it means we couldn't expand our range to the right
-            // Not possible to water whole garden
+            // Not possible to water whole garden, return -1, do not continue searching
             if (max == min)
             {
                 return -1;
             }
             
             // Now we have reached max, we want to cover more than this max
+            // Set min to max
             min = max;
             count++;
         }
