8372405bb004f749e7b9c0d6f66226c3f15a86d3
Sonya <aminisonya@gmail.com>
2022-03-23 10:56:19 -0700

    Time: 358 ms (37.58%), Space: 46.1 MB (95.18%) - LeetHub

---
+++ 344-reverse-string/344-reverse-string.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public void ReverseString(char[] s) {
+        var left = 0;
+        var right = s.Length - 1;
+        
+        while (left < right)
+        {
+            var temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            
+            left++;
+            right--;
+        }
+    }
+}
