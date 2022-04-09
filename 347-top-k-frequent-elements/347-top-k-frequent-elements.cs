9d1bc6c53744f88e9fab18eea376a6c027603d4a
Sonya <aminisonya@gmail.com>
2022-04-08 19:40:32 -0700

    Time: 136 ms (99.56%), Space: 45.9 MB (21.28%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.cs
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.cs
@@ -2,38 +2,39 @@ public class Solution {
     public int[] TopKFrequent(int[] nums, int k) {
         // Using heap approach, there are 3 main steps
 		if (nums.Length == k)
-		{
-			return nums;
-		}
+        {
+            return nums;
+        }
 		
 		// 1. Build dictionary with int + int's frequency
 		var dict = new Dictionary<int, int>();
-		for (var i = 0; i < nums.Length; i++)
-		{
-			if (dict.ContainsKey(nums[i]))
-			{
-				dict[nums[i]] = dict[nums[i]] + 1;
-			}
-			else
-			{
-				dict.Add(nums[i], 1);
-			}
-		}
+        foreach (var num in nums)
+        {
+            if (!dict.ContainsKey(num))
+            {
+                dict.Add(num, 1);
+            }
+            else
+            {
+                dict[num]++;
+            }
+        }
 		
 		// 2. Create a priority queue, based on highest frequency to lowest
-		var pQueue = new PriorityQueue<int, int>(Comparer<int>.Create((x,y) => y.CompareTo(x)));
-		foreach (var key in dict.Keys)
-		{
-			pQueue.Enqueue(key, dict[key]);
-		}
+        var pQueue = new PriorityQueue<int, int>(Comparer<int>.Create((x, y) => y.CompareTo(x)));
+        foreach (var item in dict)
+        {
+            pQueue.Enqueue(item.Key, item.Value);
+        }
+		
 		
 		// 3. Return an array with top k elements from priority queue
-		var result = new int[k];
+        var result = new int[k];
 		for (var i = 0; i < k; i++)
-		{
-			result[i] = pQueue.Dequeue();
-		}
-		
-		return result;
+        {
+            result[i] = pQueue.Dequeue();
+        }
+        
+        return result;
     }
 }
