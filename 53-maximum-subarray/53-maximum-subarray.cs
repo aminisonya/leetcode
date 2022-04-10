8b907ef0971a897f7e53f2276dd627dd8e807962
Sonya <aminisonya@gmail.com>
2022-04-09 17:44:40 -0700

    Time: 278 ms (50.06%), Space: 49.4 MB (6.51%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cs
+++ 53-maximum-subarray/53-maximum-subarray.cs
@@ -1,20 +1,19 @@
 public class Solution {
     public int MaxSubArray(int[] nums) {
-        var currSub = nums[0];
-		var maxSub = nums[0];
-		
-		for (var i = 1; i < nums.Length; i++)
-		{
-			currSub = currSub + nums[i];
-			
-			if (currSub < nums[i])
-			{
-				currSub = nums[i];
-			}
-			
-			maxSub = Math.Max(maxSub, currSub);
-		}
-		
-		return maxSub;
+        // Initialize vars with first element
+        var currMax = nums[0];
+        var max = nums[0];
+        
+        // Start from second element, we already used first element
+        for (var i = 1; i < nums.Length; i++)
+        {
+            var num = nums[i];
+            
+            // If current num is greater than current window max, update current max
+            currMax = Math.Max(num, currMax + num);
+            max = Math.Max(max, currMax);
+        }
+        
+        return max;
     }
 }
