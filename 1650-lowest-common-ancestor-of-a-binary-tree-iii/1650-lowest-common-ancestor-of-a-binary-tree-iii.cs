07ebf29459b2cf40412f15649f7c03d1653ced6e
Sonya <aminisonya@gmail.com>
2022-04-19 22:05:12 -0700

    Time: 174 ms (11.74%), Space: 40.6 MB (73.67%) - LeetHub

--- 1650-lowest-common-ancestor-of-a-binary-tree-iii/1650-lowest-common-ancestor-of-a-binary-tree-iii.cs
+++ 1650-lowest-common-ancestor-of-a-binary-tree-iii/1650-lowest-common-ancestor-of-a-binary-tree-iii.cs
@@ -11,23 +11,70 @@ public class Node {
 public class Solution {
     public Node LowestCommonAncestor(Node p, Node q)
     {
-        // Treat problem same as two linked lists
-        // Find where they intersect
-        // Create a "cycle" by interlooping the two paths from each node
+        // Find heights of both nodes
+        // Start from lower node, iterate upwards until at same height as higher node
+        // Once at equal height, iterate upwards until find a match
         
-        var runner1 = p;
-        var runner2 = q;
+        var height1 = FindHeight(p);
+        var height2 = FindHeight(q);
         
-        while (runner1 != runner2)
+        // For simplicity, always set lower node as p
+        if (height1 < height2)
         {
-            runner1 = (runner1.parent == null) ? q : runner1.parent;
-            runner2 = (runner2.parent == null) ? p : runner2.parent;
+            var temp = p;
+            p = q;
+            q = temp;
         }
         
-        return runner1;
+        var heightDiff = Math.Abs(height1 - height2);
+        while (heightDiff > 0)
+        {
+            p = p.parent;
+            heightDiff--;
+        }
+        
+        while (p != q)
+        {
+            p = p.parent;
+            q = q.parent;
+        }
+        
+        return p;
     }
     
+    public int FindHeight(Node node)
+    {
+        var height = 0;
+        
+        while (node != null)
+        {
+            height++;
+            node = node.parent;
+        }
+        
+        return height;
+    }
+}
 //     public Node LowestCommonAncestor(Node p, Node q) {
+// ----------------- Optimal Solution Option -----------------
+    
+//         //Treat problem same as two linked lists
+//         //Find where they intersect
+//         //Create a "cycle" by interlooping the two paths from each node
+        
+//         var runner1 = p;
+//         var runner2 = q;
+        
+//         while (runner1 != runner2)
+//         {
+//             runner1 = (runner1.parent == null) ? q : runner1.parent;
+//             runner2 = (runner2.parent == null) ? p : runner2.parent;
+//         }
+        
+//         return runner1;
+    
+// ----------------- Suboptimal Solution -----------------
+    
 //         // Use a hashset to store values of nodes on path from p to root
 //         // Traverse from q to root, checking for first match with hashset
 //         // That first match will be the LCA
@@ -52,4 +99,3 @@ public class Solution {
         
 //         return new Node(0);
 //     }
-}
