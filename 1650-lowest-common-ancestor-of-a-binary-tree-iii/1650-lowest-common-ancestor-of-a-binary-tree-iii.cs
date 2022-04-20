75f7519d5233638dc1c4473e8cab58fb8f3fcee5
Sonya <aminisonya@gmail.com>
2022-04-19 21:39:55 -0700

    Time: 160 ms (19.39%), Space: 40.5 MB (80.07%) - LeetHub

--- 1650-lowest-common-ancestor-of-a-binary-tree-iii/1650-lowest-common-ancestor-of-a-binary-tree-iii.cs
+++ 1650-lowest-common-ancestor-of-a-binary-tree-iii/1650-lowest-common-ancestor-of-a-binary-tree-iii.cs
@@ -9,29 +9,47 @@ public class Node {
 */
 
 public class Solution {
-    public Node LowestCommonAncestor(Node p, Node q) {
-        // Use a hashset to store values of nodes on path from p to root
-        // Traverse from q to root, checking for first match with hashset
-        // That first match will be the LCA
+    public Node LowestCommonAncestor(Node p, Node q)
+    {
+        // Treat problem same as two linked lists
+        // Find where they intersect
+        // Create a "cycle" by interlooping the two paths from each node
         
-        var hashset = new HashSet<int>();
+        var runner1 = p;
+        var runner2 = q;
         
-        while (p != null)
+        while (runner1 != runner2)
         {
-            hashset.Add(p.val);
-            p = p.parent;
+            runner1 = (runner1.parent == null) ? q : runner1.parent;
+            runner2 = (runner2.parent == null) ? p : runner2.parent;
         }
         
-        while (q != null)
-        {
-            if (hashset.Contains(q.val))
-            {
-                return q;
-            }
-            
-            q = q.parent;
-        }
-        
-        return new Node(0);
+        return runner1;
     }
+    
+//     public Node LowestCommonAncestor(Node p, Node q) {
+//         // Use a hashset to store values of nodes on path from p to root
+//         // Traverse from q to root, checking for first match with hashset
+//         // That first match will be the LCA
+        
+//         var hashset = new HashSet<int>();
+        
+//         while (p != null)
+//         {
+//             hashset.Add(p.val);
+//             p = p.parent;
+//         }
+        
+//         while (q != null)
+//         {
+//             if (hashset.Contains(q.val))
+//             {
+//                 return q;
+//             }
+            
+//             q = q.parent;
+//         }
+        
+//         return new Node(0);
+//     }
 }
