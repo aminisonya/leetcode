4cd71ac5bd47ad7bb001a1f2e685bc3f3240dfbc
Sonya <aminisonya@gmail.com>
2022-04-26 23:55:07 -0700

    Time: 68 ms (97.62%), Space: 40 MB (15.95%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.cs
+++ 567-permutation-in-string/567-permutation-in-string.cs
@@ -6,22 +6,19 @@ public class Solution {
         // Return true if all chars from s1 found in a row in s2
         // Update window when char not in s1
         
-        if (s1.Length > s2.Length || s1 == null || s2 == null)
+        if (s1.Length > s2.Length)
         {
             return false;
         }
         
-        // Iterate thru length of s1
-        // Add counts of each char in each string to respective arrays
         var s1chars = new int[26];
-        var s2chars = new int[26];        
+        var s2chars = new int[26];
         for (var i = 0; i < s1.Length; i++)
         {
             s1chars[s1[i] - 'a']++;
             s2chars[s2[i] - 'a']++;
         }
         
-        // How many chars already match up based on chars we've gone thru so far (equal to s1's length)?
         var count = 0;
         for (var i = 0; i < 26; i++)
         {
@@ -31,19 +28,17 @@ public class Solution {
             }
         }
         
-        // Sliding window of a fixed size, s1's length, sliding thru s2
         for (var i = 0; i < s2.Length - s1.Length; i++)
         {
+            var l = s2[i] - 'a';
+            var r = s2[i + s1.Length] - 'a';
+            
             if (count == 26)
             {
                 return true;
             }
             
-            var l = s2[i] - 'a';
-            var r = s2[i + s1.Length] - 'a';
-            
-            // Expand window to the right, see how it effects matching count
-            s2chars[r]++;            
+            s2chars[r]++;
             if (s2chars[r] == s1chars[r])
             {
                 count++;
@@ -53,7 +48,6 @@ public class Solution {
                 count--;
             }
             
-            // Move left side of window, see how it effects count
             s2chars[l]--;
             if (s2chars[l] == s1chars[l])
             {
