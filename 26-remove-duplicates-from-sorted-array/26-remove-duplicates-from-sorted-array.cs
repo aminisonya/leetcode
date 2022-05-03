7227cde192beee665e2eefcfda0fa90b1859405d
Sonya <aminisonya@gmail.com>
2022-05-02 22:08:45 -0700

    Time: 161 ms (74.23%), Space: 45.1 MB (38.46%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cs
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cs
@@ -4,20 +4,17 @@ public class Solution {
         // One to keep track of current num
         // Another to find next unique element that is also greater than curr num
         
-        var i = 0;
+        var i = 0; // First pointer
         for (var j = 1; j < nums.Length; j++)
         {
-            if (nums[i] == nums[j])
-            {
-                continue;
-            }
-            else if (nums[i] != nums[j])
+            // Second pointer is j
+            if (nums[i] != nums[j])
             {
                 nums[i + 1] = nums[j];
                 i++;
             }
         }
         
-        return i + 1;
+        return i + 1; // length of unique elements
     }
 }
