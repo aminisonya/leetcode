f35ba0fcfc65b129509b19a672d90ab743aa28bc
Sonya <aminisonya@gmail.com>
2022-05-02 22:24:39 -0700

    Time: 201 ms (66.05%), Space: 49 MB (40.01%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.cs
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.cs
@@ -1,14 +1,17 @@
 public class Solution {
     public int[] SortedSquares(int[] nums) {
+        // Two pointers
+        // Iterate backwards thru array
+        // Two pointers, one on each end of array
+        // This will account for negative values which will be positive once squared, and will need to be placed correctly sorted
         var result = new int[nums.Length];
         var left = 0;
         var right = nums.Length - 1;
         
-        // Can fill out result array going backwards, as we iterate inwards with two pointers
         for (var i = nums.Length - 1; i >= 0; i--)
         {
-            var rightSquare = nums[right] * nums[right];
             var leftSquare = nums[left] * nums[left];
+            var rightSquare = nums[right] * nums[right];
             
             if (rightSquare >= leftSquare)
             {
