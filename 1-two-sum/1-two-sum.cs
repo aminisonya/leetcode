92955065ae9414a1b2dbcc6db7a7f5cd710ab30a
Sonya <aminisonya@gmail.com>
2022-05-02 21:30:56 -0700

    Time: 199 ms (58.33%), Space: 44 MB (32.31%) - LeetHub

--- 1-two-sum/1-two-sum.cs
+++ 1-two-sum/1-two-sum.cs
@@ -1,26 +1,27 @@
 public class Solution {
     public int[] TwoSum(int[] nums, int target) {
-        var dict = new Dictionary<int, int>();
-		var result = new int[2];
-		
-		for (var i = 0; i < nums.Length; i++)
-		{
-			if (!dict.ContainsKey(nums[i]))
-			{
-				dict.Add(nums[i], i);
-			}
-		}
-		
-		for (var i = 0; i < nums.Length; i++)
-		{
-			var num = target - nums[i];
-			if (dict.ContainsKey(num) && dict[num] != i)
-			{
-				result[0] = i;
-				result[1] = dict[num];
-			}
-		}
-		
-		return result;
+        var result = new int[2];
+        var dict = new Dictionary<int, int>(); // number : index
+        
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (!dict.ContainsKey(nums[i]))
+            {
+                dict.Add(nums[i], i);
+            }
+        }
+        
+        for (var i = 0; i < nums.Length; i++)
+        {
+            var temp = target - nums[i];
+            
+            if (dict.ContainsKey(temp) && dict[temp] != i)
+            {
+                result[0] = dict[temp];
+                result[1] = i;
+            }
+        }
+        
+        return result;
     }
 }
