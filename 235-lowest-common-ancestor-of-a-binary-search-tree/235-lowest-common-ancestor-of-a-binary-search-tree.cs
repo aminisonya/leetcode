2899b8d05b0a985d587b63f80a300f8cde781c91
Sonya <aminisonya@gmail.com>
2022-05-22 20:05:54 -0700

    Time: 179 ms (15.26%), Space: 41.8 MB (71.23%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cs
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cs
@@ -10,19 +10,40 @@
 
 public class Solution {
     public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        var curr = root;
-		
-		if (p.val > curr.val && q.val > curr.val)
-		{
-			return LowestCommonAncestor(curr.right, p, q);
-		} else if (p.val < curr.val && q.val < curr.val)
-		{
-			return LowestCommonAncestor(curr.left, p, q);
-		} else 
-		{
-			return curr;
-		}
-		
-		return null;
+        // BST
+        // Bc it's a BST, we know we can use that for searching in O(log n) if needed
+        // We are searching for p and q, and their "split point". What node do they split at (one goes to the right, one to the left)? That's the LCA
+        // If one is parent of other, that's also LCA
+        // While loop until LCA is found
+        
+        var pVal = p.val;
+        var qVal = q.val;
+        var node = root;
+        
+        while (node != null)
+        {
+            if (node.val > pVal && node.val > qVal)
+            {
+                node = node.left;
+            }
+            else if (node.val < pVal && node.val < qVal)
+            {
+                node = node.right;
+            }
+            else if (node.val == pVal)
+            {
+                return p;
+            }
+            else if (node.val == qVal)
+            {
+                return q;
+            }
+            else
+            {
+                return node;
+            }
+        }
+        
+        return root;
     }
 }
