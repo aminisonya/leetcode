25648bc3c3c378c7f86307723faeb8449082a469
Sonya <aminisonya@gmail.com>
2022-05-31 23:44:59 -0700

    Time: 135 ms (5.79%), Space: 35.1 MB (46.58%) - LeetHub

--- 409-longest-palindrome/409-longest-palindrome.cs
+++ 409-longest-palindrome/409-longest-palindrome.cs
@@ -1,25 +1,27 @@
 public class Solution {
     public int LongestPalindrome(string s) {
         // Includes both lowercase and uppercase chars. Case sensitive.
-        // Can still use minus 'a' to see if they're equal chars or not
-        // Keep count of unique chars and repeated chars
-        // Sum of repeated chars / 2 plus one unique char (if any)
+        // Use hashset to keep count of repeated and non repeated chars
+        // If exists in hashet, you know you have 2 of that char and therefore 2 more letters to build a palindrome
+        // If doesn't exist in hashset, add it to hashset
+        // After looping thru all chars, if there is anything left in hashset, that means there are unique chars and we can add ONE to our count of longest palindrome to return
         
-        var length = 0;
         var hashset = new HashSet<char>();
-        foreach (char c in s)
+        var longestPal = 0;
+        
+        foreach (var ch in s)
         {
-            if (hashset.Contains(c))
+            if (hashset.Contains(ch))
             {
-                length += 2;
-                hashset.Remove(c);
+                longestPal += 2;
+                hashset.Remove(ch);
             }
             else
             {
-                hashset.Add(c);
+                hashset.Add(ch);
             }
         }
         
-        return hashset.Count > 0 ? length + 1 : length;
+        return hashset.Count > 0 ? longestPal + 1 : longestPal;
     }
 }
