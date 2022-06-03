abec814f157d9468449fe6efe7156b44a8877de8
Sonya <aminisonya@gmail.com>
2022-06-03 00:02:56 -0700

    Time: 184 ms (89.00%), Space: 52.8 MB (11.94%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cs
+++ 217-contains-duplicate/217-contains-duplicate.cs
@@ -1,19 +1,21 @@
 public class Solution {
     public bool ContainsDuplicate(int[] nums) {
-        var dict = new Dictionary<int, int>();
-		
-		for (var i = 0; i < nums.Length; i++)
-		{
-			if (dict.ContainsKey(nums[i]))
-			{
-				return true;
-			}
-			else
-			{
-				dict.Add(nums[i], nums[i]);
-			}
-		}
-		
-		return false;
+        // Use hashset to see if array contains duplicates
+        
+        var hashset = new HashSet<int>();
+        
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (hashset.Contains(nums[i]))
+            {
+                return true;
+            }
+            else
+            {
+                hashset.Add(nums[i]);
+            }
+        }
+        
+        return false;
     }
 }
