ea366abc00b8874dc1aa93dfb93c6c15b22638f1
Sonya <aminisonya@gmail.com>
2022-06-02 23:42:20 -0700

    Time: 131 ms (32.27%), Space: 37.8 MB (87.87%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.cs
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.cs
@@ -13,39 +13,27 @@
  */
 public class Solution {
     public int MaxDepth(TreeNode root) {
+        // Recursion + DFS
+        // Looking for farthest leaf node
+        
         if (root == null)
-		{
-			return 0;
-		}
-		
-		var max = 0;
-		var stack = new Stack<TreeNode>();
-		var depths = new Stack<int>();
-		stack.Push(root);
-		depths.Push(1);
-		
-		while (stack.Count > 0)
-		{
-			var curr = stack.Pop();
-			var depth = depths.Pop();
-			
-			if (curr.right != null)
-			{
-				stack.Push(curr.right);
-				depths.Push(depth + 1);
-			}
-			if (curr.left != null)
-			{
-				stack.Push(curr.left);
-				depths.Push(depth + 1);
-			}
-			
-			if (curr.right == null && curr.left == null)
-			{
-				max = Math.Max(max, depth);
-			}
-		}
-		
-		return max;
+        {
+            return 0;
+        }
+        
+        return FindMax(root);
+    }
+    
+    private int FindMax(TreeNode node)
+    {
+        if (node == null)
+        {
+            return 0;
+        }
+        
+        var leftMax = FindMax(node.left);
+        var rightMax = FindMax(node.right);
+        
+        return Math.Max(leftMax, rightMax) + 1;
     }
 }
