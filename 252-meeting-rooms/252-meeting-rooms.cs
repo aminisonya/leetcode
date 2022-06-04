f3ce65b7586259189d8ce75cae086731c9f652cf
Sonya <aminisonya@gmail.com>
2022-06-03 20:01:00 -0700

    Time: 195 ms (11.82%), Space: 40.6 MB (97.29%) - LeetHub

--- 252-meeting-rooms/252-meeting-rooms.cs
+++ 252-meeting-rooms/252-meeting-rooms.cs
@@ -1,27 +1,26 @@
 public class Solution {
     public bool CanAttendMeetings(int[][] intervals) {
-        // Sort
-		// Iterate and check for any overlaps
-		if (intervals.Length < 2)
-		{
-			return true;
-		}
-		
-		Array.Sort(intervals, (x,y) => x[0].CompareTo(y[0]));
-		var i = 0;
-		var j = i + 1;
-		
-		while (i < intervals.Length)
-		{
-			while (j < intervals.Length && intervals[i][1] > intervals[j][0])
-			{
-				return false;
-			}
-			
-			i = j;
-			j = i + 1;
-		}
-		
-		return true;
+        // Looking for any overlap in any of the meetings
+        // Keep track of start and end times as loop thru intervals
+        // Sort intervals so only have to iterate thru once
+        
+        if (intervals.Length < 2)
+        {
+            return true;
+        }
+        
+        // Will need to memorize how to sort 2d array
+        Array.Sort(intervals, (x,y) => x[0].CompareTo(y[0]));
+        
+        for (var i = 0; i < intervals.Length - 1; i++)
+        {
+            // Compare ending time of first meeting to starting time of next meeting
+            if (intervals[i][1] > intervals[i + 1][0])
+            {
+                return false;
+            }
+        }
+        
+        return true;
     }
 }
