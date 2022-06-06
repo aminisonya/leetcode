4514286b12c07ca91823bf2a0317dd353e63ca9e
Sonya <aminisonya@gmail.com>
2022-06-05 20:16:33 -0700

    Time: 165 ms (11.93%), Space: 38.6 MB (77.68%) - LeetHub

--- 100-same-tree/100-same-tree.cs
+++ 100-same-tree/100-same-tree.cs
@@ -13,22 +13,28 @@
  */
 public class Solution {
     public bool IsSameTree(TreeNode p, TreeNode q) {
+        // Recursion
+        // Go thru both trees and check values at each node
+        // Establish base cases
+        // These are the cases where we want to bubble up the FINAL answer
+        // Only want to bubble up the answer when we've found that the trees do not match
+        // OR when we've reached the final nodes
+        
         if (p == null && q == null)
-		{
-			return true;
-		}
-		
-		if (p == null || q == null)
-		{
-			return false;
-		}
-		
-		if (p.val != q.val)
-		{
-			return false;
-		}
-		
-		return IsSameTree(p.right, q.right) &&
-			IsSameTree(p.left, q.left);
+        {
+            return true;
+        }
+        
+        if (p == null || q == null)
+        {
+            return false;
+        }
+        
+        if (p.val != q.val)
+        {
+            return false;
+        }
+        
+        return IsSameTree(p.left, q.left) && IsSameTree(p.right, q.right);
     }
 }
