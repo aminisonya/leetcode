e78b8e3fab3baba7b2c073211eb76b2098c940f9
Sonya <aminisonya@gmail.com>
2022-06-05 21:43:07 -0700

    Time: 50 ms (5.81%), Space: 22.5 MB (47.33%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.cs
+++ 191-number-of-1-bits/191-number-of-1-bits.cs
@@ -1,22 +1,21 @@
 public class Solution {
     public int HammingWeight(uint n) {
-        // Want to return the number of '1' bits
+        // Count the number on '1' bits in n
+        // Take (n & 1) which will compare current last bit and 1 to see if it's a 1 or not. If BOTH are 1s, then last bit is a 1, and can increase count of 1's
+        // Then bit shift n to the right to cut off that last bit
+        
         var count = 0;
-
-        // Iterate through bits in n while n is not 0
+        
         while (n != 0)
         {
-          // Check if last bit is a 1
-          // If both bits are 1 (n & 1), result bit is 1
-          if ((n & 1) == 1)
-          {
-            count++;
-          }
-
-          // Right shift n by one space
-          n = n >> 1;
+            if ((n & 1) == 1)
+            {
+                count++;
+            }
+            
+            n = n >> 1;
         }
-
+        
         return count;
     }
 }
