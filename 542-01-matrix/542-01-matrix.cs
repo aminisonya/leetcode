6e50ce8bc296c0523b90bb5ba1a978241479abbc
Sonya <aminisonya@gmail.com>
2022-06-08 00:32:29 -0700

    Time: 364 ms (27.25%), Space: 50 MB (30.77%) - LeetHub

--- 542-01-matrix/542-01-matrix.cs
+++ 542-01-matrix/542-01-matrix.cs
@@ -1,19 +1,16 @@
 public class Solution {
     public int[][] UpdateMatrix(int[][] mat) {
-        // Find distance of nearest 0 for each cell
-        // BFS + Queue
-        // Queue will contain cells to be examined next
-        // Add all cells with 0s to q
-        // Pop cell from q, examine neighbors. If new distance for neighbor is smaller (orig set as int max), add to q.
+        // BFS + Queue + Hashset for visited cells
+        // Start by enqueueing all cells with 0's
+        // Check their neighbors, update value with distance from 0 for each cell
+        // Keep track of distance for each iteration round thru queue contents
         
         var rows = mat.Length;
         var cols = mat[0].Length;
-        var result = new int[rows][];
-        var queue = new Queue<(int, int)>();
+        var queue = new Queue<(int, int)>(); // Tuple within queue
         var visited = new HashSet<(int, int)>();
         
-        // Enqueue all squares with 0's
-        // Add those squares to visited hashset
+        // Add all cells with 0s to queue and visited list
         for (var i = 0; i < rows; i++)
         {
             for (var j = 0; j < cols; j++)
@@ -26,38 +23,41 @@ public class Solution {
             }
         }
         
-        // Directions to iterate thru for each cell
+        // Directions to loop thru for each cell
         var r = new int[] { 0, 1, 0, -1 };
         var c = new int[] { 1, 0, -1, 0 };
+        var distance = 0;
         
-        // level to keep track of distance from nearest 0
-        var level = 0;
         while (queue.Count > 0)
         {
             var size = queue.Count;
-            level++;
+            distance++;
             
             while (size > 0)
             {
+                // decrement size as we iterate thru queue contents
+                // Don't want to iterate thru next level of queue contents in this round
                 size--;
                 
                 var curr = queue.Dequeue();
                 
+                // Iterate thru all 4 neighbors
                 for (var d = 0; d < 4; d++)
                 {
-                    var newi = curr.Item1 + r[d];
-                    var newj = curr.Item2 + c[d];
+                    var n1 = curr.Item1 + r[d];
+                    var n2 = curr.Item2 + c[d];
                     
-                    if (newi < 0 || newj < 0 || newi >= rows || newj >= cols || visited.Contains((newi, newj)))
+                    // Check to make sure still in bounds of matrix and that cell hasn't been visited already
+                    if (n1 < 0 || n2 < 0 || n1 >= rows || n2 >= cols || visited.Contains((n1, n2)))
                     {
+                        // If so, do nothing and move on to next neighbor cell
                         continue;
                     }
                     
-                    // Set distance (level) of current cell from nearest 0
-                    // And add to queue and visited hashset
-                    queue.Enqueue((newi, newj));
-                    visited.Add((newi, newj));
-                    mat[newi][newj] = level;
+                    // Update cell value with distance! And add to queue and hashset
+                    mat[n1][n2] = distance;
+                    queue.Enqueue((n1, n2));
+                    visited.Add((n1, n2));
                 }
             }
         }
