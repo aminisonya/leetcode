d7fd89d4946bd68e2430398ead0b0d9f7b5bbe1b
Sonya <aminisonya@gmail.com>
2022-06-07 21:55:53 -0700

    Time: 315 ms (5.04%), Space: 44.6 MB (85.92%) - LeetHub

--- 57-insert-interval/57-insert-interval.cs
+++ 57-insert-interval/57-insert-interval.cs
@@ -1,34 +1,38 @@
 public class Solution {
     public int[][] Insert(int[][] intervals, int[] newInterval) {
-        // Broken into 3 steps
-		// 1. Add all intervals before the new interval starts
-		var result = new List<int[]>();
-		var i = 0;
-		
-		while (i < intervals.Length && intervals[i][1] < newInterval[0])
-		{
-			result.Add(intervals[i]);
-			i++;
-		}
-		
-		// 2. Add new interval, after merging any overlaps
-		while (i < intervals.Length && intervals[i][0] <= newInterval[1])
-		{
-			// Update values of interval to be added, that merges all potential overlaps
-			newInterval[0] = Math.Min(intervals[i][0], newInterval[0]);
-			newInterval[1] = Math.Max(intervals[i][1], newInterval[1]);
-			i++;
-		}
-		// Add new interval outside of while loop, so it's only added once
-		result.Add(newInterval);
-		
-		// 3. Add any remaining intervals
-		while (i < intervals.Length)
-		{
-			result.Add(intervals[i]);
-			i++;
-		}
-		
-		return result.ToArray();
+        // Solution in 3 steps
+        // Add all intervals ending before newinterval first
+        // Add all intervals starting after newinterval last
+        // Middle step is to merge any potential overlapping intervals, then add newinterval
+        
+        var result = new List<int[]>();
+        var i = 0;
+        
+        // Add all intervals that END BEFORE the new interval starts
+        while (i < intervals.Length && intervals[i][1] < newInterval[0])
+        {
+            result.Add(intervals[i]);
+            i++;
+        }
+        
+        // Merge new interval in, merging all overlaps
+        while (i < intervals.Length && intervals[i][0] <= newInterval[1])
+        {
+            // Want the earliest (smallest) start time
+            // And the largest (max) end time
+            newInterval[0] = Math.Min(newInterval[0], intervals[i][0]);
+            newInterval[1] = Math.Max(newInterval[1], intervals[i][1]);
+            i++;
+        }
+        result.Add(newInterval);
+        
+        // Add all intervals that START AFTER the new interval ends
+        while (i < intervals.Length && intervals[i][0] > newInterval[1])
+        {
+            result.Add(intervals[i]);
+            i++;
+        }
+        
+        return result.ToArray();
     }
 }
