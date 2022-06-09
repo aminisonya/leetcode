cc800e138b7a65bd739c1f0227efc2c9b32e3b91
Sonya <aminisonya@gmail.com>
2022-06-08 23:38:35 -0700

    Time: 189 ms (47.53%), Space: 41.8 MB (56.34%) - LeetHub

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.cs
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.cs
@@ -12,33 +12,44 @@
  * }
  */
 public class Solution {
-    public IList<IList<int>> LevelOrder(TreeNode root)
-	{
-	    var levelsList = new List<IList<int>>();
-		if (root == null)
-		{
-			return levelsList;
-		}		
-		
-		var queue = new Queue<TreeNode>();
-		queue.Enqueue(root);
-		
-		while (queue.Count > 0)
-		{
-			var size = queue.Count;
-			var list = new List<int>();
-			for (var i = 0; i < size; i++)
-			{
-				var curr = queue.Dequeue();
-				list.Add(curr.val);
-				
-				if (curr.left != null) queue.Enqueue(curr.left);
-				if (curr.right != null) queue.Enqueue(curr.right);
-			}
-			
-			levelsList.Add(list);
-		}
-		
-		return levelsList;
-	}
+    public IList<IList<int>> LevelOrder(TreeNode root) {
+        // Create a list for each level. Add list to result set.
+        // BFS + Queue
+        // While loop and inner while loop for iterating thru current level
+        
+        var result = new List<IList<int>>();
+        if (root == null)
+        {
+            return result;
+        }
+        
+        var queue = new Queue<TreeNode>();
+        queue.Enqueue(root);
+        
+        while (queue.Count > 0)
+        {
+            var size = queue.Count; // This will tell us how many are in current level
+            var list = new List<int>();
+            
+            // Inner loop to iterate thru items in this level of the tree
+            for (var i = 0; i < size; i++)
+            {                
+                var curr = queue.Dequeue();
+                list.Add(curr.val);
+                
+                if (curr.left != null)
+                {
+                    queue.Enqueue(curr.left);
+                }
+                if (curr.right != null)
+                {
+                    queue.Enqueue(curr.right);
+                }
+            }
+            
+            result.Add(list);
+        }
+        
+        return result;
+    }
 }
