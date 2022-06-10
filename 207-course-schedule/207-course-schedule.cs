aa017fcb615c45e8a796149b08a33dd5302c69e7
Sonya <aminisonya@gmail.com>
2022-06-10 16:36:35 -0700

    Time: 125 ms (78.29%), Space: 42.7 MB (71.21%) - LeetHub

--- 207-course-schedule/207-course-schedule.cs
+++ 207-course-schedule/207-course-schedule.cs
@@ -1,59 +1,59 @@
 public class Solution {
     public bool CanFinish(int numCourses, int[][] prerequisites) {
-        var adjList = new Dictionary<int, List<int>>();
-		var inDegree = new Dictionary<int, int>();
-		var visited = 0;
-		var queue = new Queue<int>();
-		
-		// Set up default adjList and inDegree
-		for (var i = 0; i < numCourses; i++)
-		{
-			adjList.Add(i, new List<int>());
-			inDegree.Add(i, 0);
-		}
-		
-		// Build graph and inDegree
-		foreach (var course in prerequisites)
-		{
-			var u = course[0];
-			var v = course[1];
-			// Add prereq to list of prereqs for that course
-			adjList[u].Add(v);
-			// Increment inDegree value for that node (the prereq)
-			inDegree[v]++;
-		}
-		
-		// Add all sources (nodes with 0 inDegree/prereq to queue)
-		foreach (var entry in inDegree)
-		{
-			if (entry.Value == 0)
-			{
-				queue.Enqueue(entry.Key);
-			}
-		}
-		
-		// BFS
-		while (queue.Count > 0)
-		{
-			// Starter node (no nodes pointing to it, we've already "crossed out" those nodes if any)
-			var parent = queue.Dequeue();
-			visited++;
-			
-			// Loop thru list of prereqs
-			foreach (var child in adjList[parent])
-			{
-				// Decrement inDegree for each child
-				inDegree[child]--;
-				
-				// If inDegree becomes zero add it to queue (it's now a "starter" node)
-				if (inDegree[child] == 0)
-				{
-					queue.Enqueue(child);
-				}
-			}
-		}
-		
-		// Did we visit every node? Then return true
-		return visited == numCourses;
+        // Graph. Topological sort algo
+        var prereqMap = new Dictionary<int, List<int>>(); // course : prereqs
+        var inDegree = new Dictionary<int, int>(); // course : number of courses pointing to it
+        var visited = 0; // keep track of how many courses we've visited
+        var queue = new Queue<int>();
+        
+        // Build prereq map, add all courses first
+        for (var i = 0; i < numCourses; i++)
+        {
+            prereqMap.Add(i, new List<int>());
+            inDegree.Add(i, 0);
+        }
+        
+        // Fill out prereq map by going thru prereqs array
+        for (var j = 0; j < prerequisites.Length; j++)
+        {
+            var course = prerequisites[j][0];
+            var prereq = prerequisites[j][1];
+            prereqMap[course].Add(prereq);
+            inDegree[prereq]++; // Add prereq bc it's pointing to a course
+        }
+        
+        // Add all courses with 0 inDegree / no prereqs to the queue
+        foreach (var course in inDegree)
+        {
+            // If that course has NO prereqs / courses pointing to it, add to queue
+            if (course.Value == 0)
+            {
+                queue.Enqueue(course.Key);
+            }
+        }
+        
+        // While loop to go level by level in the queue, adding to it as we go
+        while (queue.Count > 0)
+        {
+            var curr = queue.Dequeue(); // current course with no courses pointing to it. Not a prereq to another course.
+            visited++;
+            
+            // Go thru list of prereqs for that course
+            for (var i = 0; i < prereqMap[curr].Count; i++)
+            {
+                // decrement current prereq's indegree
+                inDegree[prereqMap[curr][i]]--;
+                
+                // If indegree reached 0, add to queue
+                // That means no courses are pointing to it anymore
+                if (inDegree[prereqMap[curr][i]] == 0)
+                {
+                    queue.Enqueue(prereqMap[curr][i]);
+                }
+            }
+        }
+        
+        // did we visit all the courses exactly once??
+        return visited == numCourses;
     }
 }
