4876ad93d66a8ae96244362fbc82386658d59b5a
Sonya <aminisonya@gmail.com>
2022-06-15 21:07:22 -0700

    Time: 480 ms (15.71%), Space: 67.1 MB (62.83%) - LeetHub

--- 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.cs
+++ 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.cs
@@ -1,61 +1,77 @@
-    public class TrieNode {
-		public bool isEnd;
-		public TrieNode[] children;
-		
-		public TrieNode() {
-			children = new TrieNode[26];
-			isEnd = false;
-		}
-	}
-	
-	public class Trie {
-		private TrieNode root;
-		
-		public Trie() {
-			root = new TrieNode();
-		}
-		
-		private TrieNode GetNode(string word)
-		{
-			var curr = root;
-			var i = 0;
-			
-			while (i < word.Length)
-			{
-				curr = curr.children[word[i] - 'a'];
-				i++;
-				if (curr == null) return null;
-			}
-			
-			return curr;
-		}
+public class Trie {
+    public TrieNode root;
 
-		public void Insert(string word) {
-			var curr = root;
-			
-			for (var i = 0; i < word.Length; i++)
-			{
-				var id = word[i] - 'a';
-				if (curr.children[id] == null)
-				{
-					curr.children[id] = new TrieNode();
-				}
-				curr = curr.children[id];
-			}
-			
-			curr.isEnd = true;
-		}
+    public Trie() {
+        root = new TrieNode();
+    }
+    
+    public void Insert(string word) {
+        var curr = root;
+        
+        for (var i = 0; i < word.Length; i++)
+        {
+            // id to represent current char
+            var id = word[i] - 'a';
+            
+            // if that char doesn't exist yet in children, create a node there
+            // then set curr to next char
+            if (curr.children[id] == null)
+            {
+                curr.children[id] = new TrieNode();
+            }
+            curr = curr.children[id];
+        }
+        
+        // Mark last letter as the end of the word
+        curr.isEnd = true;
+    }
+    
+    public bool Search(string word) {
+        var curr = root;
+        
+        for (var i = 0; i < word.Length; i++)
+        {
+            // iterate to next letter in word
+            curr = curr.children[word[i] - 'a'];
+            
+            // if letter doesn't exist, return false
+            if (curr == null)
+            {
+                return false;
+            }
+        }
+        
+        return curr.isEnd;
+    }
+    
+    public bool StartsWith(string prefix) {
+        var curr = root;
+        
+        for (var i = 0; i < prefix.Length; i++)
+        {
+            curr = curr.children[prefix[i] - 'a'];
+            
+            if (curr == null)
+            {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
 
-		public bool Search(string word) {
-			var node = GetNode(word);
-			return node != null && node.isEnd;
-		}
-
-		public bool StartsWith(string prefix) {
-			var node = GetNode(prefix);
-			return node != null;
-		}
-	}
+public class TrieNode
+{
+    public bool isEnd;
+    public TrieNode[] children;
+    
+    public TrieNode()
+    {
+        children = new TrieNode[26];
+        isEnd = false;
+    }
+}
 
 /**
  * Your Trie object will be instantiated and called as such:
