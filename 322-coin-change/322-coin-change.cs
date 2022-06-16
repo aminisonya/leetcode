5b309afa7a6a0474532b8ed880d840ab7ddee1ac
Sonya <aminisonya@gmail.com>
2022-06-15 23:56:32 -0700

    Time: 200 ms (26.88%), Space: 40.2 MB (45.01%) - LeetHub

--- 322-coin-change/322-coin-change.cs
+++ 322-coin-change/322-coin-change.cs
@@ -1,51 +1,43 @@
 public class Solution {
     public int CoinChange(int[] coins, int amount) {
-        // Using DP (memoization) and recursion (BFS)
-        
         if (amount < 1)
         {
             return 0;
         }
         
-        var cache = new int[amount];
-        
-        return MinChange(coins, amount, cache);
+        return CoinChanges(coins, amount, new int[amount]);
     }
     
-    public int MinChange(int[] coins, int amount, int[] cache)
+    private int CoinChanges(int[] coins, int rem, int[] count)
     {
-        if (amount < 0)
+        if (rem < 0)
         {
             return -1;
         }
         
-        if (amount == 0)
+        if (rem == 0)
         {
             return 0;
         }
         
-        // Check cache to see if we've already done this calculation before
-        if (cache[amount - 1] != 0)
+        if (count[rem - 1] != 0)
         {
-            return cache[amount - 1];
+            return count[rem - 1];
         }
         
-        // Set up default minimum variable as max value, to be replaced by any number smaller
         var min = Int32.MaxValue;
         
-        // Recursion thru coins in a BFS manner
         foreach (var coin in coins)
         {
-            var result = MinChange(coins, amount - coin, cache);
-            if (result >= 0 && result < min)
+            var res = CoinChanges(coins, rem - coin, count);
+            
+            if (res >= 0 && res < min)
             {
-                // Plus one bc we are acting as if each coin we subtract from total amount is the last coin used to make change
-                min = result + 1;
+                min = 1 + res;
             }
         }
         
-        // Check if min variable has been updated or not before updating cache
-        cache[amount - 1] = (min == Int32.MaxValue) ? -1 : min;
-        return cache[amount - 1];
+        count[rem - 1] = (min == Int32.MaxValue) ? -1 : min;
+        return count[rem - 1];
     }
 }
