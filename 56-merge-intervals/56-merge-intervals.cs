c46db103d8c8c16e20cd45480d24f53deac0a50d
Sonya <aminisonya@gmail.com>
2022-06-23 22:34:04 -0700

    Time: 323 ms (13.53%), Space: 46.6 MB (68.25%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cs
+++ 56-merge-intervals/56-merge-intervals.cs
@@ -1,36 +1,26 @@
 public class Solution {
     public int[][] Merge(int[][] intervals) {
-        // Start by sorting all of the intervals based on the first value
-        Array.Sort(intervals, (x, y) => x[0].CompareTo(y[0]));
-		
-		var result = new List<int[]>();
-		
-		// Iterate through subsequent intervals, starting at beginning with first two intervals
-		var i = 0;
-		var j = 1;
-		
-		// Remember to make sure we don't go out of bounds
-		while (i < intervals.Length)
-		{
-			// Grab the first and second values in the FIRST interval
-			var firstVal = intervals[i][0];
-			var secVal = intervals[i][1];
-			
-			// Compare first interval's second value to second interval's first value
-			while (j  < intervals.Length && intervals[j][0] <= secVal)
-			{
-				// Is there overlap? Then replace the first interval's second value
-				secVal = Math.Max(secVal, intervals[j][1]);
-				j++;
-			}
-			
-			// Add the newly adjusted-for-overlap interval to the result list
-			result.Add(new [] { firstVal, secVal });
-			
-			i = j;
-			j = i + 1;
-		}
-		
-		return result.ToArray();
+        // sort intervals
+        // merge overlaps when iterating
+        
+        Array.Sort(intervals, (x,y) => x[0].CompareTo(y[0]));
+        var result = new List<int[]>();
+        
+        for (var i = 0; i < intervals.Length; i++)
+        {
+            var currStart = intervals[i][0];
+            
+            if (result.Count <= 0 || result[result.Count - 1][1] < currStart)
+            {
+                result.Add(intervals[i]);
+            }
+            else if (result[result.Count - 1][1] >= currStart)
+            {
+                // merge these two intervals
+                result[result.Count - 1][1] = Math.Max(result[result.Count - 1][1], intervals[i][1]);
+            }
+        }
+        
+        return result.ToArray();
     }
 }
