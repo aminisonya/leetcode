d17e29765fedbcd53b820997cc6fad9434131c89
Sonya <aminisonya@gmail.com>
2022-09-15 13:37:31 -0700

    Time: 263 ms (41.02%), Space: 44.2 MB (14.44%) - LeetHub

--- 1-two-sum/1-two-sum.cs
+++ 1-two-sum/1-two-sum.cs
@@ -1,24 +1,26 @@
 public class Solution {
     public int[] TwoSum(int[] nums, int target) {
-        // Dictionary
-        // Two pass thrus
+        // iterate thru the array and build a dictionary. number : index
+        // iterate again thru the array, subtract each number from the target, and see if the dict has that value
+        // when it does, you've found the two that add up to the target. create the result array and return that
         
-        var dict = new Dictionary<int, int>(); // number : index
-        var result = new int[2];
-        
+        var dict = new Dictionary<int, int>();
         for (var i = 0; i < nums.Length; i++)
         {
             if (!dict.ContainsKey(nums[i]))
             {
                 dict.Add(nums[i], i);
             }
-            
-            var currTarget = target - nums[i];
-            
-            if (dict.ContainsKey(currTarget) && dict[currTarget] != i)
+        }
+        
+        var result = new int[2];
+        for (var i = 0; i < nums.Length; i++)
+        {
+            var numNeeded = target - nums[i];
+            if (dict.ContainsKey(numNeeded) && i != dict[numNeeded])
             {
                 result[0] = i;
-                result[1] = dict[currTarget];
+                result[1] = dict[numNeeded];
             }
         }
         
