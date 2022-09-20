132985143fa7bd2e32f03a9951d4d60137695504
Sonya <aminisonya@gmail.com>
2022-09-20 09:54:23 -0700

    Time: 642 ms (5.02%), Space: 47.3 MB (33.96%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cs
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cs
@@ -1,24 +1,22 @@
 public class Solution {
     public int MaxProfit(int[] prices) {
-        // two pointer approach. non-sorted array.
-        // keep track of max profit seen so far
-        // min price seen so far and current price used to calc max profit
+        // two pointer approach
+        // unsorted array
+        // keep track of max profit and min priceseen so far
         
         var maxProfit = 0;
-        var minPrice = int.MaxValue;
+        var minPrice = Int32.MaxValue;
         
         for (var i = 0; i < prices.Length; i++)
         {
-            var curr = prices[i];
-            
             if (prices[i] < minPrice)
             {
                 minPrice = prices[i];
             }
             
-            maxProfit = Math.Max(maxProfit, curr - minPrice);
+            maxProfit = Math.Max(maxProfit, prices[i] - minPrice);
         }
-                
+        
         return maxProfit;
     }
 }
